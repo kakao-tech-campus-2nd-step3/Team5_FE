bdf4ae41f8e8b3b4423508cb3187190993bd979a
hyunaeri <tjgus9650@gmail.com>
2024-09-20 20:52:14 +0900

    Env: MSW 환경 세팅 완료

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,17 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
-);
+async function deferRender() {
+  if (import.meta.env.VITE_RUN_MSW === 'true') {
+    const { worker } = await import('./mocks/browser');
+    await worker.start();
+  }
+}
+
+deferRender().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
+});
