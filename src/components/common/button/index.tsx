dce86183350e445e356d8a9e62d3d59f8cc049a1
KimKyuHoi <k546kh@gmail.com>
2024-09-26 02:07:44 +0900

    Feat: 버튼 공용 컴포넌트 구현

---
+++ src/components/common/button/index.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+
+import { cva, type VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils';
+import { Slot } from '@radix-ui/react-slot';
+
+const buttonVariants = cva(
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        default:
+          'bg-primary text-primary-foreground shadow hover:bg-secondary hover:text-secondary-foreground',
+        destructive:
+          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
+        secondary:
+          'bg-secondary text-secondary-foreground shadow-sm hover:bg-primary hover:text-primary-foreground',
+        ghost: 'hover:bg-accent hover:text-accent-foreground',
+      },
+      size: {
+        default: 'h-10 rounded-md text-base w-full',
+        sm: 'h-10 rounded-xl text-base w-full',
+        lg: 'h-14 rounded-xl w-full text-xl',
+        tag: 'h-10 pt-3 pb-3 pl-4 pr-4 rounded-2xl text-base',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'button';
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = 'Button';
+
+export { Button, buttonVariants };
