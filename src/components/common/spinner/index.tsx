246ba68c12587948d894df05200f1903d7dd42e8
hyunaeri <tjgus9650@gmail.com>
2024-09-26 13:29:36 +0900

    Feat: AsyncBoundary 를 적용한 라우터 설정

---
+++ src/components/common/spinner/index.tsx
@@ -0,0 +1,27 @@
+import { keyframes } from 'styled-components';
+import styled from 'styled-components';
+
+type Props = {
+  size?: number;
+};
+export const Spinner = ({ size = 24 }: Props) => {
+  return <Wrapper size={size} />;
+};
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Wrapper = styled.div<Pick<Props, 'size'>>`
+  border: 4px solid rgba(177, 177, 177, 0.01);
+  width: ${({ size }) => size}px;
+  height: ${({ size }) => size}px;
+  border-radius: 50%;
+  border-left-color: #999;
+  animation: ${spin} 1s linear infinite;
+`;
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,17 +1,45 @@
-import { createBrowserRouter } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  IndexRouteObject,
+  NonIndexRouteObject,
+} from 'react-router-dom';
+
+import { AsyncBoundary } from '@/components/common/AsyncBoundary';
+import { LoadingView } from '@/components/common/view/LoadingView';
 
 import ErrorPage from '@/pages/ErrorPage';
 import RootPage from '@/pages/RootPage';
 import MainPage from '@/pages/main/MainPage';
 
-export const Router: ReturnType<typeof createBrowserRouter> = createBrowserRouter([
+type AppRouteObject = (IndexRouteObject | NonIndexRouteObject) & {
+  children?: AppRouteObject[];
+};
+
+const routesConfig: AppRouteObject[] = [
   {
     path: '/',
     element: <RootPage />,
     id: 'root',
     errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <MainPage /> }
-    ],
+    children: [{ index: true, element: <MainPage /> }],
   },
-]);
+];
+
+const routesWithAsyncBoundary = (
+  routes: AppRouteObject[]
+): AppRouteObject[] => {
+  return routes.map((route) => {
+    const { element, children, ...rest } = route;
+    return {
+      ...rest,
+      element: (
+        <AsyncBoundary pendingFallback={<LoadingView />}>
+          {element}
+        </AsyncBoundary>
+      ),
+    } as AppRouteObject;
+  });
+};
+
+export const Router: ReturnType<typeof createBrowserRouter> =
+  createBrowserRouter(routesWithAsyncBoundary(routesConfig));
