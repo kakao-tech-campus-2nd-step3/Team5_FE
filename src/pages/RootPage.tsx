64b5f7241296c68870eae7cf6b54b9bb8f61a26d
hyunaeri <tjgus9650@gmail.com>
2024-10-04 13:13:15 +0900

    Feat: 사이드바 컴포넌트 구현
    - 파일이름 TALKAK_Logo.png → Logo.png로 변경
    - 공용 컴포넌트 Button 일부 수정 (아이콘을 선택적으로 추가 가능)
    - 사이드바 컴포넌트 추가

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
+
+import Sidebar from '@/components/feature/sidebar';
 
 const RootPage = () => {
   return (
     <>
-      <div>RootPage</div>
+      <Sidebar />
       <Outlet />
     </>
   );
