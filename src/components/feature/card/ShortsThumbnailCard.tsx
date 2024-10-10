503562aa0fc52de38a2da4d1cf33240b6e7a9a69
hyunaeri <tjgus9650@gmail.com>
2024-10-10 15:50:45 +0900

    Feat: ShortsThumbnailCard 컴포넌트 구현

---
+++ src/components/feature/card/ShortsThumbnailCard.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+import { LazyLoadImg } from '@/components/common/img/LazyLoadingImg';
+
+interface ShortsCardProps {
+  image: {
+    alt: string;
+    src: string;
+  };
+  title: string;
+  timeAgo: string;
+  category?: string;
+}
+
+const ShortsThumbnailCard: React.FC<ShortsCardProps> = ({
+  image,
+  title,
+  timeAgo,
+  category,
+}) => {
+  return (
+    <Card>
+      <LazyLoadImg image={image} />
+      <Title>{title}</Title>
+      {category && <div>{category}</div>}
+      <TimeAgo>{timeAgo}</TimeAgo>
+    </Card>
+  );
+};
+
+export default ShortsThumbnailCard;
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 180px;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  margin: 10px;
+  padding-top: 10px;
+`;
+
+const Title = styled.h3`
+  font-size: 16px;
+  margin: 10px 0;
+  text-align: center;
+`;
+
+const TimeAgo = styled.p`
+  font-size: 14px;
+  color: #666;
+  margin-top: 10px;
+  margin-bottom: 10px;
+`;
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,9 +1,20 @@
 import Header from '@/components/feature/header'
+import ShortsThumbnailCard from '@/components/feature/card/ShortsThumbnailCard';
+
+import shortsImg from '@/assets/shorts_img.png';
 
 const MainPage = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <ShortsThumbnailCard 
+        image={ { src: shortsImg, alt: 'shorts' } }
+        title='인기 쇼츠1'
+        timeAgo='1시간 전'
+        category='스포츠'
+      />
+    </>
   )
 };
 
-export default MainPage
+export default MainPage;
