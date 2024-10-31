928c61ad98bb5e72e485a9602332ee118b97af1c
KimKyuHoi <k546kh@gmail.com>
2024-10-31 16:15:50 +0900

    chore: 필요없는 Import 제거

--- src/components/feature/sidebar/index.tsx
+++ src/components/feature/sidebar/index.tsx
@@ -9,7 +9,6 @@ import {
   FaBasketballBall,
 } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 
 import styled from 'styled-components';
 
