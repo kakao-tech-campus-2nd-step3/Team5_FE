0dd20b3232ad54b953995af6d87bce916216ac95
KimKyuHoi <k546kh@gmail.com>
2024-11-07 15:24:13 +0900

    feat: api 입력했을떄 사용자 명 및 url 영상 뜨도록 구현

--- src/pages/auto/components/field/LinkField.tsx
+++ src/pages/auto/components/field/LinkField.tsx
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import { UseFormReturn } from 'react-hook-form';
 
+import { debounce } from 'lodash';
 import { z } from 'zod';
 
 import {
@@ -11,26 +13,64 @@ import {
   FormMessage,
 } from '@/components';
 
+import { postValidateUrl } from '@/pages/auto/apis';
+import { useLinkContext } from '@/pages/auto/provider';
 import { FormSchema } from '@/pages/auto/utils';
 
 type LinkFieldProps = {
   form: UseFormReturn<z.infer<typeof FormSchema>>;
 };
 
-const LinkField = ({ form }: LinkFieldProps) => (
-  <FormField
-    control={form.control}
-    name='link'
-    render={({ field }) => (
-      <FormItem>
-        <FormLabel>Youtube Link</FormLabel>
-        <FormControl>
-          <Input type='url' placeholder='링크를 입력하세요.' {...field} />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    )}
-  />
-);
+const LinkField = ({ form }: LinkFieldProps) => {
+  const { watch, setError, clearErrors } = form;
+  const linkValue = watch('link');
+
+  const { setLinkState } = useLinkContext();
+
+  useEffect(() => {
+    const validateUrl = debounce(async (url: string) => {
+      try {
+        const data = await postValidateUrl({ url });
+
+        setLinkState({
+          title: data.title,
+          user: data.user,
+          url: data.url,
+        });
+
+        clearErrors('link');
+      } catch (error) {
+        setError('link', {
+          type: 'manual',
+          message: '유효하지 않은 URL입니다.',
+        });
+      }
+    }, 500);
+
+    if (linkValue) {
+      validateUrl(linkValue);
+    }
+
+    return () => {
+      validateUrl.cancel();
+    };
+  }, [linkValue, setError, clearErrors]);
+
+  return (
+    <FormField
+      control={form.control}
+      name='link'
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Youtube Link</FormLabel>
+          <FormControl>
+            <Input type='url' placeholder='링크를 입력하세요.' {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
 
 export default LinkField;
