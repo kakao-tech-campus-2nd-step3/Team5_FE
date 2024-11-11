65801e3fabe248bdd994b6930f1b78b5d955cef6
hyunaeri <tjgus9650@gmail.com>
2024-11-11 21:57:49 +0900

    Feat: Mocking API 를 위한 PostCommentsHandler 작성

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -2,5 +2,6 @@ import { AutoHandler } from './auto';
 import { FetchShortsHandler } from './main';
 import { ViewShortsHandler } from './viewer';
 import { FetchCommentsHandler } from './viewer';
+import { PostCommentsHandler } from './viewer';
 
-export const handlers = [...AutoHandler, ...FetchShortsHandler, ...ViewShortsHandler, ...FetchCommentsHandler];
+export const handlers = [...AutoHandler, ...FetchShortsHandler, ...ViewShortsHandler, ...FetchCommentsHandler, ...PostCommentsHandler];
