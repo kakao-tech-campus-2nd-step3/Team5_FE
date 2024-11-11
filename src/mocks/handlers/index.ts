93eb3e5cee0a495b8e23651e668b66e3e5532ffd
hyunaeri <tjgus9650@gmail.com>
2024-11-11 16:18:55 +0900

    Feat: Mocking API 를 위한 FetchCommentsHandler 작성

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,5 +1,6 @@
 import { AutoHandler } from './auto';
 import { FetchShortsHandler } from './main';
 import { ViewShortsHandler } from './viewer';
+import { FetchCommentsHandler } from './viewer';
 
-export const handlers = [...AutoHandler, ...FetchShortsHandler, ...ViewShortsHandler];
+export const handlers = [...AutoHandler, ...FetchShortsHandler, ...ViewShortsHandler, ...FetchCommentsHandler];
--- src/mocks/handlers/viewer/index.ts
+++ src/mocks/handlers/viewer/index.ts
@@ -1,1 +1,2 @@
 export { ViewShortsHandler } from './ViewShortsHandler';
+export { FetchCommentsHandler } from './FetchCommentsHandler';
