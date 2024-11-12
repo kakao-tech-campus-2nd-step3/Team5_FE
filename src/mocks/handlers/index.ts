5c3967a659667237bd3872769a5bf14aac99f180
hyunaeri <tjgus9650@gmail.com>
2024-11-12 13:15:27 +0900

    Rename: 파일 경로 변경

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,8 +1,17 @@
 import { AddHandler } from '@/pages/add/mocks';
 import { AutoHandler } from '@/pages/auto/mocks';
 import { FetchShortsHandler } from '@/pages/main/mocks';
-import { ViewShortsHandler } from './viewer';
-import { FetchCommentsHandler } from './viewer';
-import { PostCommentsHandler } from './viewer';
+import {
+  FetchCommentsHandler,
+  PostCommentsHandler,
+  ViewShortsHandler,
+} from '@/pages/viewer/mocks';
 
-export const handlers = [...AddHandler, ...AutoHandler, ...FetchShortsHandler, ...ViewShortsHandler, ...FetchCommentsHandler, ...PostCommentsHandler];
+export const handlers = [
+  ...AddHandler,
+  ...AutoHandler,
+  ...FetchShortsHandler,
+  ...ViewShortsHandler,
+  ...FetchCommentsHandler,
+  ...PostCommentsHandler,
+];
