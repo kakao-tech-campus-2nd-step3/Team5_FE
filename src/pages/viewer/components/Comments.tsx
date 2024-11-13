019c43669fa27239d2a4b8ebe8e252c36782000a
hyunaeri <tjgus9650@gmail.com>
2024-11-13 20:54:32 +0900

    Feat: 댓글 수정 및 삭제 기능 추가

--- src/pages/viewer/components/Comments.tsx
+++ src/pages/viewer/components/Comments.tsx
@@ -1,38 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import { useCreateComment } from '@/pages/viewer/apis/useCreateComment.api';
-import {
-  useFetchComments,
-  Comment,
-} from '@/pages/viewer/apis/useFetchComments.api';
 import * as Styles from '@/pages/viewer/components/Comments.style';
+import { useComments } from '@/pages/viewer/hooks/useComments';
 
 interface CommentsContainerProps {
   onClose: () => void;
   videoId: number;
 }
 
-const CommentsContainer: React.FC<CommentsContainerProps> = ({ onClose, videoId }) => {
+const CommentsContainer: React.FC<CommentsContainerProps> = ({
+  onClose,
+  videoId,
+}) => {
   const {
-    data: fetchedComments = [],
+    comments,
+    newComment,
+    setNewComment,
+    editingCommentId,
+    editContent,
+    setEditContent,
+    showOptions,
+    setEditingCommentId,
+    handleCommentSubmit,
+    handleEditSubmit,
+    handleDeleteComment,
+    toggleOptions,
     isLoading,
     error,
-  } = useFetchComments(videoId);
-  const [comments, setComments] = useState<Comment[]>(fetchedComments);
-  const [newComment, setNewComment] = useState('');
-  const createCommentMutation = useCreateComment();
-
-  const handleCommentSubmit = () => {
-    createCommentMutation.mutate(
-      { videoId, content: newComment },
-      {
-        onSuccess: (createdComment) => {
-          setComments((prevComments) => [...prevComments, createdComment]); 
-          setNewComment(''); 
-        },
-      }
-    );
-  };
+  } = useComments(videoId);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error loading comments.</div>;
@@ -52,8 +47,41 @@ const CommentsContainer: React.FC<CommentsContainerProps> = ({ onClose, videoId 
               />
               <Styles.CommentContent>
                 <Styles.UserName>{comment.member.username}</Styles.UserName>
-                <Styles.Description>{comment.content}</Styles.Description>
+                {editingCommentId === comment.commentId ? (
+                  <Styles.CommentInput
+                    value={editContent}
+                    onChange={(e) => setEditContent(e.target.value)}
+                    onKeyDown={(e) =>
+                      e.key === 'Enter' && handleEditSubmit(comment.commentId)
+                    }
+                  />
+                ) : (
+                  <Styles.Description>{comment.content}</Styles.Description>
+                )}
               </Styles.CommentContent>
+              <Styles.OptionsButton
+                onClick={() => toggleOptions(comment.commentId)}
+              >
+                ⋮
+              </Styles.OptionsButton>
+              {showOptions === comment.commentId && (
+                <Styles.OptionsMenu>
+                  <Styles.OptionItem
+                    onClick={() => {
+                      setEditingCommentId(comment.commentId);
+                      setEditContent(comment.content);
+                      toggleOptions(comment.commentId);
+                    }}
+                  >
+                    수정하기
+                  </Styles.OptionItem>
+                  <Styles.OptionItem
+                    onClick={() => handleDeleteComment(comment.commentId)}
+                  >
+                    삭제하기
+                  </Styles.OptionItem>
+                </Styles.OptionsMenu>
+              )}
             </Styles.Comment>
           ))
         ) : (
