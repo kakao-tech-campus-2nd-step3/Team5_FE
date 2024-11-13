f459fcb818119447b9703dcb5a9fda9616ffc739
hyunaeri <tjgus9650@gmail.com>
2024-11-14 01:04:49 +0900

    Chore: ShortsVideoProps 속성 변경

--- src/pages/main/apis/fetchShortsList.ts
+++ src/pages/main/apis/fetchShortsList.ts
@@ -3,11 +3,11 @@ import { fetchInstance } from '@/apis/instance';
 import { useQuery } from '@tanstack/react-query';
 
 export type ShortsVideoProps = {
-  videoId: number;
+  video_id: number;
   thumbnail: string;
   title: string;
-  memberId: number;
-  createdAt: string;
+  member_id: number;
+  created_at: string;
 };
 
 export type FetchShortsParams = {
@@ -34,10 +34,7 @@ export const fetchShortsByCategory = async (
 
 export const useFetchShortsByCategory = (params: FetchShortsParams) => {
   return useQuery<ShortsVideoProps[]>({
-    queryKey: [
-      'fetchShortsByCategory',
-      params.categoryId
-    ],
+    queryKey: ['fetchShortsByCategory', params.categoryId],
     queryFn: () => fetchShortsByCategory(params),
     staleTime: 1000 * 60 * 3,
   });
