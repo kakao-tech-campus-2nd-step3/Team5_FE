214a53edefcb8dc04a6b0b2dd9e8e501b2f29948
hyunaeri <tjgus9650@gmail.com>
2024-11-14 01:05:54 +0900

    Chore: spy 속성 제거

--- src/components/feature/sidebar/index.tsx
+++ src/components/feature/sidebar/index.tsx
@@ -114,7 +114,6 @@ const Sidebar = () => {
           <Link
             to={label}
             smooth={true}
-            spy={true}
             duration={500}
             offset={-80}
             containerId='scrollContainer'
