553a25e4beed3e98fb420751acd9c280f18143ac
KimKyuHoi <k546kh@gmail.com>
2024-11-14 03:23:17 +0900

    feat: fast api url 설정

--- src/constants/URI.ts
+++ src/constants/URI.ts
@@ -1,1 +1,2 @@
 export const BASE_URL = import.meta.env.VITE_API_URL;
+export const BASE_FAST_URL = import.meta.env.VITE_FAST_API_URL;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
 export { USER_AGENT_REGEX } from './regex';
-export { BASE_URL } from './URI';
+export { BASE_URL, BASE_FAST_URL } from './URI';
