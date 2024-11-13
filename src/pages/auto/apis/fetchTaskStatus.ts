71cfd2347fe463de4df3f92eb8771456a19e5102
KimKyuHoi <k546kh@gmail.com>
2024-11-14 03:28:17 +0900

    feat: polling api 구현 및 데이터 수정

---
+++ src/pages/auto/apis/fetchTaskStatus.ts
@@ -0,0 +1,29 @@
+import { fetchFastInstance } from '@/apis/instance';
+
+import { useQuery } from '@tanstack/react-query';
+
+export type fetchTaskStatusResponseProps = {
+  task_id: string;
+  status: string;
+};
+
+const fetchTaskStatusPath = (task_id: string) => `/task-status/${task_id}`;
+
+const fetchTaskStatus = async (
+  task_id: string
+): Promise<fetchTaskStatusResponseProps> => {
+  const response = await fetchFastInstance.get<fetchTaskStatusResponseProps>(
+    fetchTaskStatusPath(task_id)
+  );
+  return response.data;
+};
+
+export const usefetchTaskStatus = (task_id: string) => {
+  const accessToken = localStorage.getItem('accessToken');
+
+  return useQuery<fetchTaskStatusResponseProps, Error>({
+    queryKey: [fetchTaskStatusPath],
+    queryFn: () => fetchTaskStatus(task_id),
+    enabled: !!accessToken,
+  });
+};
