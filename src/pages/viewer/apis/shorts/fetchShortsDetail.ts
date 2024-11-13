8fc27a70b32db990c7de5594a7d819a08639ef19
hyunaeri <tjgus9650@gmail.com>
2024-11-14 01:07:08 +0900

    Refactor: 외부 인터페이스 가져오기

--- src/pages/viewer/apis/shorts/fetchShortsDetail.ts
+++ src/pages/viewer/apis/shorts/fetchShortsDetail.ts
@@ -1,31 +1,18 @@
+import { ShortsCardProps } from '@/pages/viewer/components/ShortsCard';
+
 import { fetchInstance } from '@/apis/instance/Instance.api';
 
 import { useQuery } from '@tanstack/react-query';
 
-export interface ShortsProps {
-  id: number;
-  title: string;
-  categoryId: number;
-  videoUrl: string;
-  memberInfo: {
-    id: number;
-    imageUrl: string;
-    username: string;
-  };
-  likeCount: number;
-  viewCount: number;
-  commentsCount: number;
-}
-
 const fetchShortsDetailPath = (videoId: number) => `/api/videos/${videoId}`;
 
-const fetchShortById = async (videoId: number): Promise<ShortsProps> => {
+const fetchShortById = async (videoId: number): Promise<ShortsCardProps> => {
   const response = await fetchInstance.get(fetchShortsDetailPath(videoId));
   return response.data;
 };
 
 export const useFetchShortDetail = (videoId: number) => {
-  return useQuery<ShortsProps, Error>({
+  return useQuery<ShortsCardProps, Error>({
     queryKey: ['short', videoId],
     queryFn: () => fetchShortById(videoId),
     staleTime: 1000 * 60 * 3,
