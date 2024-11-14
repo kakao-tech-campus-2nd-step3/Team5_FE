c58b95a7a3ca519f67f78e1654b460a2fec5923b
hyunaeri <tjgus9650@gmail.com>
2024-11-15 01:43:08 +0900

    Feat: 내 정보 수정 API 로직 추가

--- src/pages/myPage/components/ProfileEditForm.tsx
+++ src/pages/myPage/components/ProfileEditForm.tsx
@@ -1,10 +1,13 @@
+import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 
 import styled from 'styled-components';
 import { z } from 'zod';
 
-import { Form, Button } from '@/components';
+import { Form, Button, Spinner } from '@/components';
 
+import { useFetchMyInfo } from '@/pages/myPage/apis/fetchMyInfo';
+import { useUpdateMyInfo } from '@/pages/myPage/apis/updateMyInfo';
 import ProfileEditField from '@/pages/myPage/components/ProfileEditField';
 import { ProfileSchema } from '@/pages/myPage/utils/ProfileSchema';
 
@@ -12,7 +15,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 
 type ProfileFormValues = z.infer<typeof ProfileSchema>;
 
-const ProfileEditForm = () => {
+const ProfileEditForm = React.memo(() => {
   const form = useForm<ProfileFormValues>({
     resolver: zodResolver(ProfileSchema),
     defaultValues: {
@@ -22,10 +25,56 @@ const ProfileEditForm = () => {
     },
   });
 
+  const { mutate: updateMyInfo } = useUpdateMyInfo();
+  const { data: myInfo, isLoading } = useFetchMyInfo();
+
+  console.log('My Info: ', myInfo);
+
+  useEffect(() => {
+    if (!isLoading && myInfo) {
+      const defaultValues = {
+        gender: myInfo.gender as '남자' | '여자' | undefined,
+        age: myInfo.age as
+          | '10대'
+          | '20대'
+          | '30대'
+          | '40대'
+          | '50대 이상'
+          | undefined,
+        category: myInfo.categories.map(
+          (cat) => cat.name as '음식' | '여행' | '게임' | '음악' | '스포츠'
+        ),
+      };
+      form.reset(defaultValues);
+    }
+  }, [myInfo, isLoading]);
+
   const onSubmit = (data: ProfileFormValues) => {
-    console.log('Form submitted');
-    console.log(data);
+    const categoryMapping: { [key: string]: number } = {
+      음식: 1,
+      여행: 2,
+      게임: 3,
+      음악: 4,
+      스포츠: 5,
+    };
+
+    const payload = {
+      gender: data.gender || '',
+      age: data.age || '',
+      categories: data.category.map((cat) => categoryMapping[cat]),
+    };
+
+    updateMyInfo(payload, {
+      onSuccess: (response: any) => {
+        console.log('정보가 성공적으로 업데이트되었습니다:', response);
+      },
+      onError: (error: any) => {
+        console.error('정보 업데이트에 실패하였습니다:', error);
+      },
+    });
   };
+
+  if (isLoading) return <Spinner />;
 
   return (
     <Form {...form}>
@@ -40,7 +89,7 @@ const ProfileEditForm = () => {
       </form>
     </Form>
   );
-};
+});
 
 export default ProfileEditForm;
 
