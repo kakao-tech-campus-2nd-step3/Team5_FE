5724f7d0be4d231217bc2909b8656c35f3bca006
hyunaeri <tjgus9650@gmail.com>
2024-11-15 01:37:15 +0900

    Chore: 명세에 맞게 일부 수정

--- src/pages/myPage/components/ProfileEditField.tsx
+++ src/pages/myPage/components/ProfileEditField.tsx
@@ -39,14 +39,11 @@ const ProfileEditField = ({ form }: ProfileEditFieldProps) => {
                   </CommonUI.SelectTrigger>
                   <CommonUI.SelectContent>
                     <CommonUI.SelectGroup>
-                      <CommonUI.SelectItem value='남성'>
-                        남성
+                      <CommonUI.SelectItem value='남자'>
+                        남자
                       </CommonUI.SelectItem>
-                      <CommonUI.SelectItem value='여성'>
-                        여성
-                      </CommonUI.SelectItem>
-                      <CommonUI.SelectItem value='그 외'>
-                        그 외
+                      <CommonUI.SelectItem value='여자'>
+                        여자
                       </CommonUI.SelectItem>
                     </CommonUI.SelectGroup>
                   </CommonUI.SelectContent>
@@ -106,11 +103,11 @@ const ProfileEditField = ({ form }: ProfileEditFieldProps) => {
                   isMulti
                   options={options}
                   defaultValue={options.filter((option) =>
-                    field.value.includes(option.value)
+                    (field.value as string[]).includes(option.label)
                   )}
                   onChange={(selectedOptions) => {
                     field.onChange(
-                      selectedOptions.map((option) => option.value)
+                      selectedOptions.map((option) => option.label)
                     );
                   }}
                 />
