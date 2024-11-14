f4a3d7b769a90257372828bef64a98d0456412d1
hyunaeri <tjgus9650@gmail.com>
2024-11-15 01:43:18 +0900

    Feat: 더미 데이터

--- src/pages/myPage/components/VideoManagement.tsx
+++ src/pages/myPage/components/VideoManagement.tsx
@@ -7,7 +7,7 @@ const VideoManagement = () => {
     <>
       <SectionTitle>영상 관리</SectionTitle>
       <VideoSection>
-        <ShortsGrid />
+        <ShortsGrid categoryId={1} />
       </VideoSection>
     </>
   );
