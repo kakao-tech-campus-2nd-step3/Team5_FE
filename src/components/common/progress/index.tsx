3eacf91412f0dec6ecfbd0417e0eaf297893e6cd
KimKyuHoi <k546kh@gmail.com>
2024-11-14 16:53:33 +0900

    design: progress bar애니메이션 추가 및 skeleton 추가

--- src/components/common/progress/index.tsx
+++ src/components/common/progress/index.tsx
@@ -16,7 +16,10 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className='h-full w-full flex-1 bg-primary transition-all'
+      className={cn(
+        'h-full w-full flex-1 transition-all',
+        'animate-flowing-water bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500'
+      )}
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
