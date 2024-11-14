cd6e8dbf4eebe190fef6d56bbac540e8e3e648cb
hyunaeri <tjgus9650@gmail.com>
2024-11-15 02:32:30 +0900

    Style: 스타일 수정

--- src/pages/myPage/components/ProfileEditForm.tsx
+++ src/pages/myPage/components/ProfileEditForm.tsx
@@ -18,58 +18,60 @@ interface ProfileEditFormProps {
   refetchMyInfo: () => void;
 }
 
-const ProfileEditForm: React.FC<ProfileEditFormProps> = React.memo(({ refetchMyInfo }) => {
-  const form = useForm<ProfileFormValues>({
-    resolver: zodResolver(ProfileSchema),
-    defaultValues: {
-      gender: undefined,
-      age: undefined,
-      category: [],
-    },
-  });
-
-  const { mutate: updateMyInfo } = useUpdateMyInfo();
-
-  const onSubmit = (data: ProfileFormValues) => {
-    const categoryMapping: { [key: string]: number } = {
-      음식: 1,
-      여행: 2,
-      게임: 3,
-      음악: 4,
-      스포츠: 5,
-    };
-
-    const payload = {
-      gender: data.gender || '',
-      age: data.age || '',
-      categories: data.category.map((cat) => categoryMapping[cat]),
-    };
-
-    updateMyInfo(payload, {
-      onSuccess: (response: any) => {
-        console.log('정보가 성공적으로 업데이트되었습니다:', response);
-        refetchMyInfo();
-      },
-      onError: (error: any) => {
-        console.error('정보 업데이트에 실패하였습니다:', error);
+const ProfileEditForm: React.FC<ProfileEditFormProps> = React.memo(
+  ({ refetchMyInfo }) => {
+    const form = useForm<ProfileFormValues>({
+      resolver: zodResolver(ProfileSchema),
+      defaultValues: {
+        gender: undefined,
+        age: undefined,
+        category: [],
       },
     });
-  };
 
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <FormContainer>
-          <SectionTitle>개인정보 수정</SectionTitle>
-          <ProfileEditField form={form} />
-          <Button variant='default' type='submit'>
-            수정하기
-          </Button>
-        </FormContainer>
-      </form>
-    </Form>
-  );
-});
+    const { mutate: updateMyInfo } = useUpdateMyInfo();
+
+    const onSubmit = (data: ProfileFormValues) => {
+      const categoryMapping: { [key: string]: number } = {
+        음식: 1,
+        여행: 2,
+        게임: 3,
+        음악: 4,
+        스포츠: 5,
+      };
+
+      const payload = {
+        gender: data.gender || '',
+        age: data.age || '',
+        categories: data.category.map((cat) => categoryMapping[cat]),
+      };
+
+      updateMyInfo(payload, {
+        onSuccess: (response: any) => {
+          console.log('정보가 성공적으로 업데이트되었습니다:', response);
+          refetchMyInfo();
+        },
+        onError: (error: any) => {
+          console.error('정보 업데이트에 실패하였습니다:', error);
+        },
+      });
+    };
+
+    return (
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormContainer>
+            <SectionTitle>개인정보 수정</SectionTitle>
+            <ProfileEditField form={form} />
+            <Button variant='default' type='submit'>
+              수정하기
+            </Button>
+          </FormContainer>
+        </form>
+      </Form>
+    );
+  }
+);
 
 export default ProfileEditForm;
 
@@ -83,6 +85,6 @@ const FormContainer = styled.div`
 const SectionTitle = styled.h3`
   font-size: 24px;
   font-weight: bold;
-  margin-top: 40px;
-  margin-bottom: 24px;
+  margin-top: 16px;
+  margin-bottom: 16px;
 `;
--- src/pages/myPage/components/VideoManagement.tsx
+++ src/pages/myPage/components/VideoManagement.tsx
@@ -19,11 +19,14 @@ const VideoManagement = () => {
 
 export default VideoManagement;
 
-const SectionTitle = styled.h3`
-  font-size: 20px;
+const SectionTitle = styled.h2`
+  font-size: 24px;
   font-weight: bold;
   margin-top: 40px;
   margin-bottom: 20px;
 `;
 
-const VideoSection = styled.div``;
+const VideoSection = styled.div`
+  max-width: 1400px;
+  margin: 0 auto;
+`;
