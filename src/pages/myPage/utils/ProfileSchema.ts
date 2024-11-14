5f345cffc527bdd6459c57a4d6c6068e57bbadbd
hyunaeri <tjgus9650@gmail.com>
2024-11-15 01:43:32 +0900

    Chore: 잘못된 스키마 일부 수정

--- src/pages/myPage/utils/ProfileSchema.ts
+++ src/pages/myPage/utils/ProfileSchema.ts
@@ -1,13 +1,15 @@
 import { z } from 'zod';
 
 export const ProfileSchema = z.object({
-  gender: z.enum(['남성', '여성', '그 외'], {
+  gender: z.enum(['남자', '여자'], {
     errorMap: () => ({ message: '성별을 선택해주세요.' }),
   }),
   age: z.enum(['10대', '20대', '30대', '40대', '50대 이상'], {
     errorMap: () => ({ message: '연령대를 선택해주세요.' }),
   }),
-  category: z.array(z.string()).min(1, {
-    message: '선호 카테고리를 선택해주세요.',
-  }),
+  category: z
+    .array(z.enum(['음식', '여행', '게임', '음악', '스포츠']))
+    .length(3, {
+      message: '선호 카테고리를 정확히 세 개 선택해주세요.',
+    }),
 });
