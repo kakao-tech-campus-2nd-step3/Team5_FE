b88f1ca974e31b2046407f2160153ff1b85ae4d5
hyunaeri <tjgus9650@gmail.com>
2024-11-15 01:44:00 +0900

    Feat: 댓글 불러오기 API 에 refetch 추가

--- src/pages/viewer/apis/comments/fetchComments.ts
+++ src/pages/viewer/apis/comments/fetchComments.ts
@@ -1,5 +1,4 @@
 import { fetchInstance } from '@/apis/instance/Instance.api';
-
 import { useQuery } from '@tanstack/react-query';
 
 export interface Comment {
@@ -12,8 +11,7 @@ export interface Comment {
   content: string;
 }
 
-const fetchCommentsPath = (videoId: number) =>
-  `/api/videos/${videoId}/comments`;
+const fetchCommentsPath = (videoId: number) => `/api/videos/${videoId}/comments`;
 
 export const fetchComments = async (videoId: number): Promise<Comment[]> => {
   const response = await fetchInstance.get(fetchCommentsPath(videoId));
@@ -21,10 +19,12 @@ export const fetchComments = async (videoId: number): Promise<Comment[]> => {
 };
 
 export const useFetchComments = (videoId: number) => {
-  return useQuery<Comment[], Error>({
+  const { data, isLoading, error, refetch } = useQuery<Comment[], Error>({
     queryKey: ['comments', videoId],
     queryFn: () => fetchComments(videoId),
     enabled: !!videoId,
     staleTime: 1000 * 60 * 5,
   });
+
+  return { data, isLoading, error, refetch };
 };
