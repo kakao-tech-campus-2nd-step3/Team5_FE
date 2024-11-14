004e490688a48c2318f4282e05f50d2cec8e7f4d
KimKyuHoi <k546kh@gmail.com>
2024-11-14 17:14:08 +0900

    feat: select-highlight api 구현

--- src/pages/auto/apis/index.ts
+++ src/pages/auto/apis/index.ts
@@ -2,3 +2,5 @@ export { postValidateUrl } from './postValidateUrl.api';
 export type { ValidateUrlProps, UrlOnlyProps } from './postValidateUrl.api';
 export { postConvertForm } from './postConvertForm.api';
 export { usefetchTaskStatus } from './fetchTaskStatus';
+export { fetchSelectHighlight } from './fetchSelectHighLight.api';
+export type { fetchSelectHighlightResponseProps } from './fetchSelectHighLight.api';
