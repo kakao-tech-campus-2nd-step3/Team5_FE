2028e6c9456c6e758702a5657e3d243286856f2b
hyunaeri <tjgus9650@gmail.com>
2024-11-15 15:13:02 +0900

    Fix: size 재조정

--- src/pages/main/apis/fetchShortsList.ts
+++ src/pages/main/apis/fetchShortsList.ts
@@ -25,7 +25,7 @@ export const fetchShortsByCategory = async (
     params: {
       categoryId: params.categoryId,
       page: params.page ?? 0,
-      size: params.size ?? 100,
+      size: params.size ?? 30,
     },
   });
 
